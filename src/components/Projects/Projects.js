b2c582d3508043228c187fdc6f25a0c7ec44cc26
NARISIMHA143 <narisimhaguntamukkala.gmail.com>
2024-08-11 15:28:13 +0530

    x

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -35,7 +35,6 @@ function Projects() {
               title="Portfolio Website"
               description="My personal portfolio."
               ghLink="https://github.com/NARISIMHA143/portfolio"
-              demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
@@ -46,8 +45,7 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/NARISIMHA143/Face_N_Emotion_Detection" 
             />
           </Col>
         </Row>
